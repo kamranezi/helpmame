48e89c37a3ef3b3042befa4e95e2c25f4388d79d
alexrus1144 <alexrus1144@gmail.com>
2025-09-14 09:40:37 +0000

    Без функций

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Button from './components/Button';
+import Image from 'next/image';
 
 export default function HomePage() {
   return (
@@ -10,6 +11,14 @@ export default function HomePage() {
         <Button href="/consultation">Записаться на консультацию</Button>
         <Button href="/urgent" variant="red">Срочная онлайн помощь</Button>
       </div>
+      <Image
+        src="/banner.jpeg"
+        alt="Banner"
+        width={1024}
+        height={1024}
+        className="mx-auto block"
+      />
+      
     </div>
   );
 }
