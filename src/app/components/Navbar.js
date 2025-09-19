7846a060ada9706f70ee5c05a3b450da6da32eb5
alexrus1144 <alexrus1144@gmail.com>
2025-09-19 15:18:45 +0000

    Розе и стрелка

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -2,12 +2,13 @@
 import Link from 'next/link';
 import AuthButtons from './AuthButtons';
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import Image from 'next/image';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
+  const pathname = usePathname();
 
   const navLinkClass = "hover:text-gray-200";
 
@@ -24,14 +25,16 @@ export default function Navbar() {
   );
 
   return (
-    <nav className="bg-purple-500 text-white p-4 flex justify-between items-center relative font-semibold">
+    <nav className="bg-rose-500 text-white p-4 flex justify-between items-center relative font-semibold">
       {/* Left Side: Back Button & Logo */}
       <div className="flex items-center space-x-4">
-        <button onClick={() => router.back()} className="text-white hover:text-gray-200">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-          </svg>
-        </button>
+        {pathname !== '/' && (
+          <button onClick={() => router.back()} className="text-white hover:text-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+            </svg>
+          </button>
+        )}
         <Link href="/">
           <div className="flex items-center space-x-2 cursor-pointer">
             <Image src="/logo.jpg" alt="HelpMame Logo" width={40} height={40} className="rounded-full" />
@@ -75,7 +78,7 @@ export default function Navbar() {
       </div>
 
       {/* Mobile Menu (collapsible) */}
-      <div className={`absolute top-full left-0 w-full bg-purple-500 md:hidden ${isOpen ? 'block' : 'hidden'}`}>
+      <div className={`absolute top-full left-0 w-full bg-rose-400 md:hidden ${isOpen ? 'block' : 'hidden'}`}>
         <ul className="flex flex-col items-center space-y-4 p-4">
           {navLinks}
         </ul>
